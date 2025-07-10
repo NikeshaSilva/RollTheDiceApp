436af87c0e01c8e4315e8646d309edde01993c46
Nikesha Silva <nikesha.silva@innovative-e.com>
2025-07-10 15:45:30 +0530

    Added minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Text, StyleSheet, View } from 'react-native';
 import React, { Component } from 'react';
+import ImageLayout from './components/ImageLayout';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>App</Text>
+      <View style={styles.baseViewStyles}>
+        <ImageLayout />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  baseViewStyles: {
+    flex: 1,
+  },
+});
